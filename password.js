bad8236b4ab6b3937b02daf08975f7b105107cfe
Ethan Sharp <ethansharpdev@gmail.com>
2022-04-15 12:53:10 -0600

    almost done with password

--- password.js
+++ password.js
@@ -6,14 +6,17 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-console.log("Welcome to the password validator tool!")
+console.log("Welcome to the password validator tool!");
 
-reader.question("Please enter a password for us to validate with at least 10 characters: ", 
-    function(input) {
 
-    if(input.length >= 10) {
+reader.question("Please enter a password for us to validate with at least 10 characters: " function(input)
+    userInput = input('');
+
+    if(userInput.length >= 10) {
         console.log("Great! Password is valid!")
-    }else if(input.length < 10) {
+    }else if(userInput.length < 10) {
         console.log("Oops! Password is invalid.")
     }
+
+    reader.close();
 }
