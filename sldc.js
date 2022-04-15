57b5dfd20d0fd0006efdbbf83ef4ff5c701fd929
Ethan Sharp <ethansharpdev@gmail.com>
2022-04-15 11:52:29 -0600

    working sdlc definitions

--- sldc.js
+++ sldc.js
@@ -19,3 +19,13 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = ""
+
+const design = ""
+
+const implementation = ""
+
+const testingAndIntegration = ""
+
+const mantain = ""
