3377e800e210263386e0a4aaaa498734ebb9adb0
Ethan Sharp <ethansharpdev@gmail.com>
2022-04-15 12:08:17 -0600

    sdlc working on defs

--- sldc.js
+++ sldc.js
@@ -20,12 +20,12 @@
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
 
-const analysisOfRequirements = ""
+const analysisOfRequirements = "In the second step, developers will gather information about what the project will require through brainstorming and asking questions."
 
-const design = ""
+const design = "The third step will be using the requirements attained through step 2 to design a system and to decided what languages will best fit the system for what the project needs."
 
-const implementation = ""
+const implementation = "The fourth step is where the coding begins. Code is written and piece by piece is getting ready to be implemented to the project."
 
-const testingAndIntegration = ""
+const testingAndIntegration = "On step five, actual integration of the written code takes place, and developers begin to debug and report issues so the program can be used."
 
-const mantain = ""
+const mantain = "The project is done and made availible for users "
